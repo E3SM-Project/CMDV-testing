c6f018b0365108d17d370791af434fb445aecb78
Andreas Wilke <wilke@mcs.anl.gov>
2018-02-01 16:31:27 -0600

    Added function for single step execution and step output processing

--- scripts/cmdv-test-runner.py
+++ scripts/cmdv-test-runner.py
@@ -5,10 +5,12 @@ import git
 import json
 import logging
 import os
+import glob
 import re
 import shutil
 import subprocess
 import sys
+import importlib
 from pprint import pprint
 import Archive
 import Report
@@ -30,7 +32,149 @@ import xml.etree.ElementTree as xmlet
 # print("Hello: "+eval(x))
 
 
+def execute_command(cmd , config=None , step_report=None) :
+  """cmd is an array"""
+  
+  if not step_report :
+    logger.error("No report object")
+    sys.exit(1)
+  
+  if not cmd :
+    logger.error("No command to execute")
+  else:  
+    command = " ".join(cmd)
+    logger.info("Executing: " + command )
+    
+    process = subprocess.Popen( cmd ,  stdout=subprocess.PIPE , stderr=subprocess.PIPE , shell=True)
+    output , errs = process.communicate()
+    
+    if output :
+      logger.info("Output:" + output.decode())
+      step_report.message = output.decode()
+    if errs :
+      logger.error( output.decode() )
+      logger.error( errs.decode() )
+      step_report.error = errs.decode()
+    
+    step_report.status = "success"
+      
+    if process.returncode :
+      step_report.status = "failed"
+    if step_report.error :
+      step_report.status = "failed"
+    if step_report.message and re.search("test_failed" , step.message) :
+      step_report.status = "failed" 
+    
+      
+def execute_step(step , environment=None) :
+  """ Execute script defined in step
 
+        "baseCommand" : ["ls"] ,
+        "arguments" : ["-l" , ">" , "listing.txt"] ,
+        "inputs" : null ,
+        "outputs" : {
+          "summary" : {
+            "type" : "File" ,
+            "outputBinding" : {
+              "glob" : "listing.txt"
+            }
+          }
+        }  
+  """
+  
+  current_env = os.environ
+  if environment and isinstance(environment , dict) :
+    for k in environment:
+      if k == "PATH" :
+        paths = ":".join(environment['PATH'])
+        current_env['PATH'] = current_env['PATH'] + ":" + paths 
+      else :
+        current_env['k'] = environment[k]    
+    
+  receipt = { 
+    "stderr" : None ,
+    "stdout" : None ,
+    "exit_status" : None ,
+  }
+  
+  # build cli
+  cmd = []
+  if 'baseCommand' in step :    
+    for part in step['baseCommand'] :
+      cmd.append(part)
+  else:
+    logger.error("No command to run")
+    sys.exit(1)
+    
+  if 'arguments' in step and isinstance(step['arguments'] , list ):
+    for part in step['arguments'] :
+      cmd.append(part)
+          
+  process = subprocess.Popen( cmd ,  stdout=subprocess.PIPE , stderr=subprocess.PIPE , shell=True)
+  output , errs = process.communicate()
+  
+  if output :
+    logger.info("Output:" + output.decode())
+    receipt['stdout'] = output.decode()
+  if errs :
+    logger.error( errs.decode() )
+    receipt['stdout'] = errs.decode()
+  receipt['exit_status'] = process.returncode 
+  
+  
+  # gather output
+  if step['outputs'] and isinstance(step['outputs'], dict) :
+      for k in step['outputs'] :
+        output = step['outputs'][k]
+        if output['type']== "File" :
+          if 'outputBinding' in output:
+            if "glob" in output['outputBinding']:
+              files = glob.glob( output['outputBinding']['glob'] )
+              receipt[k] = []
+              for f in files :
+                receipt[k].append(os.path.abspath(f))
+            else:
+              logger.debug("No glob in outputBinding for " + k)
+          else:  
+            logger.debug("No outputBinding for " + k)
+            pass
+        else:
+          logger.debug("Unknown type " + k['type'] + " - not implemented")  
+  return receipt       
+      
+def create_test_summary(receipt) :
+  
+  # call test_checker module here
+  # for now go through output and count "ERROR" strings
+  
+  # "stderr" : None ,
+  # "stdout" : None ,
+  # "exit_status" : None ,
+  
+  for k in receipt :
+    
+    if receipt[k] :
+      if isinstance(receipt[k] , list) :
+        for entry in receipt[k] :
+          # test if file and grep errors in file
+          pass
+      else:
+        if os.path.isfile(receipt[k]) :
+          # test if file and grep errors in file
+          pass
+        else:
+          # grep errors in text
+          results = re.findall(receipt[k], "ERROR")  
+          if results and len(results) :
+            logger.debug("Found " + len(results) + " errors.")
+          else:
+            logger.debug("Found no errors.")
+            pass
+    else:
+      logger.debug('No value for ' + k)   
+  sys.exit(1)
+  pass       
+      
 def deploy(current_config , repo=None , branch=None , base_dir=None , command="git clone" ):
   """docstring for fname"""
   
@@ -87,6 +231,8 @@ def deploy(current_config , repo=None , branch=None , base_dir=None , command="g
           logger.error( output.decode() )
           logger.error( errs.decode() )
         
+        logger.debug("Return code = " + str(process.returncode) )
+      
      
           
   else:
@@ -185,8 +331,7 @@ def build(config):
   # Go back to top level dir
   os.chdir(current_dir)
   
-
-def run(config):
+def run(config , step_report=None):
   """docstring for fname"""
   
   
@@ -222,6 +367,11 @@ def run(config):
   if not run_dir:
     run_dir = build_dir
   
+  # Change into run directory  
+  current_dir = os.getcwd()
+  logger.debug("current path " + current_dir)
+  logger.debug("changing to " + run_dir )
+  os.chdir(run_dir)
   
   command = run['run'] 
   path    = None
@@ -229,31 +379,33 @@ def run(config):
   if run['relative_path_to_run_command'] :
     bin_path = build_dir + run['relative_path_to_run_command']
   
-  current_dir = os.getcwd()
-  
-  logger.debug("current path " + current_dir)
-  logger.debug("changing to " + run_dir )
-  
-  os.chdir(run_dir)
-  
-  command = bin_path + "/" + run['run'] 
-  
-  logger.info("Executing:\t" + command)
-  process = subprocess.Popen([command],  stdout=subprocess.PIPE , stderr=subprocess.PIPE , shell=False)
-  output , errs = process.communicate()
-  if output :
-    logger.info("Output:" + output.decode())
-  if errs :
-    logger.error( output.decode() )
-    logger.error( errs.decode() )
-  pass
-  
-  pass  
+  if 'baseCommand' in run :    
+    receipt = execute_step(run , environment={ "PATH" : [bin_path] }) 
+    summary = create_test_summary(receipt)
+  else:
+    # support for older style - deprecated 
+    command = bin_path + "/" + run['run']
 
+    execute_command([command] , config=config , step_report=step_report)
+
+    logger.info("Executing:\t" + command)
+    process = subprocess.Popen([command],  stdout=subprocess.PIPE , stderr=subprocess.PIPE , shell=False)
+    output , errs = process.communicate()
+    if output :
+      logger.info("Output:" + output.decode())
+    if errs :
+      logger.error( output.decode() )
+      logger.error( errs.decode() )
+   
+  # Change back into current dir
+  os.chdir(current_dir)
+  
+  
+ 
+  
 def compare(config):
   """docstring for fname"""
   pass  
-
 
 def postproc(config):
   pass
@@ -265,7 +417,7 @@ def main(config=None):
   """docstring for fname"""
   
   # Setup
-  logger.info('Starting process')
+  logger.info('Starting session')
   
   # Test for working directory
   tmp_dir = args.dir
@@ -308,11 +460,9 @@ def main(config=None):
     
     
     for step in ['deploy' , 'build' , 'run' , 'postproc'] :
-    
       logger.debug('Executing step:\t' + step) 
     
-      if (args.step == 'all' or args.step == step ) :
-        
+      if (args.step == 'all' or args.step == step ) :      
         if step in test['steps'] : 
           
           # init test report for step
@@ -331,16 +481,17 @@ def main(config=None):
             build(test)
           if (step == 'run') :
             logger.info('Run step')
-            run(test)
+            run(test , step_report=report_for_current_step)
           if (step == 'postproc') :
             logger.info('Postprocessing step')
             postproc(test)
 
 
 
-  logger.info("Checking archiving settings")
-  if (args.archive or  "archive" in global_config) :
-    
+  
+  
+  if (args.archive or  ( "archive" in global_config and global_config['archive']['execute'])) :
+    logger.info("Archiving session")
     
     module_name="Archive." + global_config['archive']['module']
   
@@ -352,16 +503,13 @@ def main(config=None):
     archive = MyClass(logger_name="test-runner" , config = config)
     
     
-    logger.info('Archiving test run')
-    archive.source = "/tmp/testing/run/"
-    archive.logfile = "coag_rate.out"
-    logger.info("Pushing")  
-    pprint(report.toDict())
-    pprint(report.tests)
+    # archive.source = "/tmp/testing/run/"
+    # archive.logfile = "coag_rate.out"
+
     archive.push(report = report) 
   else:
-    pprint(global_config)
     logger.info("Not archiving session")  
+    pprint(report.toDict())
 
   os.chdir(current_dir)    
   pass  
@@ -375,7 +523,8 @@ def main(config=None):
 
 # Logging
 
-logger = getLogger(__name__)
+# logger = getLogger(__name__)
+logger = getLogger("cmdv-test-runner")
 logger.info("Setup")
 
 
@@ -430,14 +579,14 @@ if __name__ == "__main__":
     logger.error("Could not find global config")  
 
   
-  module_name="Archive.CDash"
-  
-  # Standard import
-  import importlib
-  # Load "module.submodule.MyClass"
-  MyClass = getattr(importlib.import_module(module_name), "CDash")
-  # Instantiate the class (pass arguments to the constructor, if needed)
-  archive = MyClass(logger_name="test-runner")
+  # module_name="Archive.CDash"
+#
+#   # Standard import
+#   import importlib
+#   # Load "module.submodule.MyClass"
+#   MyClass = getattr(importlib.import_module(module_name), "CDash")
+#   # Instantiate the class (pass arguments to the constructor, if needed)
+#   archive = MyClass(logger_name="test-runner")
   
  
 
