00f9b1960e909e7e3cd1dff71d11904ba4d4190f
Andreas Wilke <wilke@mcs.anl.gov>
2018-02-20 13:55:05 -0600

    Added PYTHONPATH

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ repo_dir=`dirname "$0"`
 current_dir=`pwd`
 
 export JENKINS_HOME=${current_dir}/${repo_dir}/Jenkins
+export PYTHONPATH=$PYTHONPATH:${current_dir}/${repo_dir}/lib/python
 # Setting up persistant Jenkins dir
 mkdir -p $JENKINS_HOME
 
