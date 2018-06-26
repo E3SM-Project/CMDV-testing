49f0a816766488281bf3c1756460416965ca9123
gabarne <gabarne@sandia.gov>
2018-06-26 16:24:01 -0600

    Small fix to how path and pythonpath are modified in init.bat

--- init.bat
+++ init.bat
@@ -1,15 +1,19 @@
 @echo off
 
+echo.
 echo Setting PATH and PYTHONPATH
+echo.
 
 set current_dir=%cd%
 
 REM export JENKINS_HOME=${current_dir}/${repo_dir}/Jenkins
-echo PYTHONPATH=%PYTHONPATH%;%current_dir%\lib\python
-set  PYTHONPATH=%PYTHONPATH%;%current_dir%\lib\python
+echo PYTHONPATH=%PYTHONPATH%%current_dir%\lib\python;
+set  PYTHONPATH=%PYTHONPATH%%current_dir%\lib\python;
+echo.
 
-echo PATH=%PATH%;%current_dir%\scripts
-set  PATH=%PATH%;%current_dir%\scripts
+echo PATH=%PATH%%current_dir%\scripts;
+set  PATH=%PATH%%current_dir%\scripts;
+echo.
 
 REM Setting up persistant Jenkins dir
 REM mkdir -p $JENKINS_HOME
