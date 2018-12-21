0c4f7cadca875308fa63a0309918ddbaf12de80e
Irina K. Tezaur <ikalash@sandia.gov>
2018-12-20 17:36:45 -0800

    Settings paths in unittest-discovery.test.yaml based on cmake configuration variables.
    All 4 unit tests now run to completion with cmake/cdash scripts.
    
    Still need to modify logic such that failure is reported to ctest in the
    case something goes wrong with the tests.  Currently if all scripts execute,
    ctest thinks the tests passed.

--- tests/set_paths.sh
+++ tests/set_paths.sh
@@ -6,5 +6,10 @@ echo "CMDV_PY_PATH ="$CMDV_PY_PATH
 CLONE_DIR_PATH=$2
 echo "CLONE_DIR_PATH ="$CLONE_DIR_PATH
 
+PY_DIR=$3
+echo "PY_DIR ="$PY_DIR
+
 sed -i -e "s,'clone-dir.sh',aaaaaa,g" $CMDV_PY_PATH/CMDV.py
 sed -i -e "s,aaaaaa,'$CLONE_DIR_PATH/clone-dir.sh',g" $CMDV_PY_PATH/CMDV.py
+sed -i -e "s,../lib/python/,bbbbbb,g" unittest-discovery.test.yaml
+sed -i -e "s,bbbbbb,$PY_DIR,g" unittest-discovery.test.yaml
