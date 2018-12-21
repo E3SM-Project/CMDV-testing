2dc56f7d25d4bbe82f0cb5edab45b242e722eed3
Irina K. Tezaur <ikalash@sandia.gov>
2018-12-20 20:55:43 -0800

    Putting in logic for ctest to fail if all 4 tests do not execute correctly.

--- tests/runtest.cmake
+++ tests/runtest.cmake
@@ -2,16 +2,29 @@
 MESSAGE("CLONE_DIR_PATH=${CLONE_DIR_PATH}")
 MESSAGE("CLONE_DIR_PATH=${PY_DIR}")
 
+set(LOGFILE log.txt)
+
 EXECUTE_PROCESS(COMMAND bash set_paths.sh ${PY_DIR} ${CLONE_DIR_PATH} 
     RESULT_VARIABLE ERROR1)
 if(ERROR1)
         message(FATAL_ERROR "Error setting paths!")
 endif()
 
-EXECUTE_PROCESS(COMMAND bash runUnitTest.sh ${PY_PATH}
+EXECUTE_PROCESS(COMMAND bash runUnitTest.sh ${PY_PATH} ${LOGFILE}
     RESULT_VARIABLE ERROR)
 if(ERROR)
         message(FATAL_ERROR "Test failed!")
 endif()
+
+EXECUTE_PROCESS(COMMAND bash check_error.sh ${LOGFILE}
+    RESULT_VARIABLE LOG_ERROR)
+if(LOG_ERROR)
+        message(FATAL_ERROR "Test failed!")
+endif()
+
+EXECUTE_PROCESS(COMMAND cat
+          INPUT_FILE ${LOGFILE}
+          RESULT_VARIABLE CAT_ERROR)
+
 
 
