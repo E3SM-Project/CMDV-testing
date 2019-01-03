c9e8b9710692afb671445dbbca31c07a9d23e855
Jason Sarich <sarich@mcs.anl.gov>
2019-01-03 13:36:14 -0600

    rename log file everywhere

--- dashboards/jenkins.lcrc.anl.gov/process_results.sh
+++ dashboards/jenkins.lcrc.anl.gov/process_results.sh
@@ -1,17 +1,17 @@
 echo "processing results"
-if [ ! -f nightly_log.txt ]; then
-    echo "process_results.sh: ERROR: file nightly_log.txt not found"
+if [ ! -f test_log.txt ]; then
+    echo "process_results.sh: ERROR: file test_log.txt not found"
 fi
-grep "Test   #" nightly_log.txt >& results0
-grep "Test  #" nightly_log.txt >& results1 
+grep "Test   #" test_log.txt >& results0
+grep "Test  #" test_log.txt >& results1 
 cat results0 results1 >& results11
-grep "Test #" nightly_log.txt >& results0
+grep "Test #" test_log.txt >& results0
 cat results11 results0 >& results1
-grep " tests failed" nightly_log.txt >& results2 
+grep " tests failed" test_log.txt >& results2 
 cat results1 results2 >& results3
-grep "Total Test" nightly_log.txt >& results4
+grep "Total Test" test_log.txt >& results4
 cat results3 results4 >& results5
-grep "(Failed)" nightly_log.txt >& results6 
+grep "(Failed)" test_log.txt >& results6 
 cat results5 results6 >& results
 echo "" >> results 
 echo "The ACME_Climate CDash site can be accessed here: https://my.cdash.org/index.php?project=ACME_Climate" >> results 
--- dashboards/jenkins.lcrc.anl.gov/run_cdash.sh
+++ dashboards/jenkins.lcrc.anl.gov/run_cdash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . ./init.sh #add to pythonpath and path
 cd dashboards/jenkins.lcrc.anl.gov
-LOG_FILE=nightly_log.txt
+LOG_FILE=test_log.txt
 eval "bash do_cmake" > $LOG_FILE 2>&1
 eval "env TEST_DIRECTORY=$PWD SCRIPT_DIRECTORY=$PWD ctest -VV -s $PWD/ctest_jenkins.cmake" >> $LOG_FILE 2>&1
 bash process_results.sh
