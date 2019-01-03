f7d322abd0c84fe04d9c90fad778f9abb44385c3
Jason Sarich <sarich@mcs.anl.gov>
2019-01-03 12:34:21 -0600

    add cmake to script

--- dashboards/jenkins.lcrc.anl.gov/run_cdash.sh
+++ dashboards/jenkins.lcrc.anl.gov/run_cdash.sh
@@ -2,5 +2,6 @@
 . ./init.sh #add to pythonpath and path
 cd dashboards/jenkins.lcrc.anl.gov
 LOG_FILE=nightly_log.txt
-eval "env TEST_DIRECTORY=$PWD SCRIPT_DIRECTORY=$PWD ctest -VV -s $PWD/ctest_jenkins.cmake" > $LOG_FILE 2>&1
+eval "bash do_cmake" > $LOG_FILE 2>&1
+eval "env TEST_DIRECTORY=$PWD SCRIPT_DIRECTORY=$PWD ctest -VV -s $PWD/ctest_jenkins.cmake" >> $LOG_FILE 2>&1
 bash process_results.sh
