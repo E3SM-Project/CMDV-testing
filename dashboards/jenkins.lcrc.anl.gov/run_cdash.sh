e1b82719c6c76876802b095dd877a5e1c5a1ff62
Jason Sarich <sarich@mcs.anl.gov>
2019-01-03 11:12:27 -0600

    fix run_cdash.sh

--- dashboards/jenkins.lcrc.anl.gov/run_cdash.sh
+++ dashboards/jenkins.lcrc.anl.gov/run_cdash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . ./init.sh #add to pythonpath and path
-cd dashboards/jenkins.lcrc.anl.gov/run_cdash.sh
+cd dashboards/jenkins.lcrc.anl.gov
 LOG_FILE=nightly_log.txt
 eval "env TEST_DIRECTORY=$PWD SCRIPT_DIRECTORY=$PWD ctest -VV -s $PWD/ctest_jenkins.cmake" > $LOG_FILE 2>&1
 bash process_results.sh
