dbcfa4a89784974f1587d5fb74d970ee2547be80
Jason Sarich <sarich@mcs.anl.gov>
2019-01-03 10:54:29 -0600

    add jenkins cdash testing on jenkins.lcrc.anl.gov

---
+++ dashboards/jenkins.lcrc.anl.gov/run_cdash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+. ./init.sh #add to pythonpath and path
+cd dashboards/jenkin.lcrc.anl.gov/run_cdash.sh
+LOG_FILE=nightly_log.txt
+eval "env TEST_DIRECTORY=$PWD SCRIPT_DIRECTORY=$PWD ctest -VV -s $PWD/ctest_jenkins.cmake" > $LOG_FILE 2>&1
+bash process_results.sh
