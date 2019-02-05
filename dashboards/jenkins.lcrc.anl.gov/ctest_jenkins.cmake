e43ef72befc3b2219ef55dcc1980e2dda87bdbf4
Jason Sarich <sarich@mcs.anl.gov>
2019-02-05 11:40:27 -0600

    branch not available here

--- dashboards/jenkins.lcrc.anl.gov/ctest_jenkins.cmake
+++ dashboards/jenkins.lcrc.anl.gov/ctest_jenkins.cmake
@@ -78,10 +78,10 @@ if (DOWNLOAD)
   #
   # Get CMDV-Testing repo 
   #
-
+  
   if (NOT EXISTS "${CTEST_SOURCE_DIRECTORY}/CMDV-Testing")
     execute_process (COMMAND "${CTEST_GIT_COMMAND}" 
-      clone ${CMDVTesting_REPOSITORY_LOCATION} -b sarich/jenkins ${CTEST_SOURCE_DIRECTORY}/CMDV-Testing
+      clone ${CMDVTesting_REPOSITORY_LOCATION}  ${CTEST_SOURCE_DIRECTORY}/CMDV-Testing
       OUTPUT_VARIABLE _out
       ERROR_VARIABLE _err
       RESULT_VARIABLE HAD_ERROR)
