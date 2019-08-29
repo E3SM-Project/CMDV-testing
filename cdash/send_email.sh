1b299d4fe951cc260dd16e330d87520e8f4f3add
Irina K. Tezaur <ikalash@sandia.gov>
2019-08-29 15:53:50 -0700

    Adding send_email.sh script with correct subject (cori not edison).

---
+++ cdash/send_email.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#source $1 
+
+TTT=`grep "(Failed)" nightly_log.txt -c`
+TTTT=`grep "(Not Run)" nightly_log.txt -c`
+TTTTT=`grep "(Timeout)" nightly_log.txt -c`
+TT=`grep "...   Passed" nightly_log.txt -c`
+
+/bin/mail -s "cmdv-test-runner, cori.nersc.gov: $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, wilke.anl@gmail.com" -F "Irina Tezaur" < results
+#/bin/mail -s "cmdv-test-runner, cori.nersc.gov: $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov" -F "Irina Tezaur" < results
