4a4f175fe8896cd193170a083f984662316c14fd
Irina K. Tezaur <ikalash@sandia.gov>
2019-11-07 14:54:16 -0800

    Updating scripts for cori.  Enabling cronjob on cori.

--- dashboards/cori.nersc.gov/modules.sh
+++ dashboards/cori.nersc.gov/modules.sh
@@ -2,3 +2,4 @@ module purge
 module load cmake
 module load git
 module load python/3.6-anaconda-5.2
+module list
--- dashboards/cori.nersc.gov/send_email.sh
+++ dashboards/cori.nersc.gov/send_email.sh
@@ -7,4 +7,4 @@ TTTT=`grep "(Not Run)" nightly_log.txt -c`
 TTTTT=`grep "(Timeout)" nightly_log.txt -c`
 TT=`grep "...   Passed" nightly_log.txt -c`
 
-/bin/mail -s "cmdv-test-runner, edison.nersc.gov: $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov" -F "Irina Tezaur" < results
+/bin/mail -s "cmdv-test-runner, cori.nersc.gov: $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov" -F "Irina Tezaur" < results
